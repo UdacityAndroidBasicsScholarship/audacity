d3ea7110b9793511efc11866c336dbee94166ffa
sabdar <developer.sabdar@gmail.com>
2018-04-13 08:43:10 +0530

    updating

--- app/src/main/java/com/udacity/audacity/MainActivity.java
+++ app/src/main/java/com/udacity/audacity/MainActivity.java
@@ -3,6 +3,8 @@ package com.udacity.audacity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.loginusingsqlite.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
