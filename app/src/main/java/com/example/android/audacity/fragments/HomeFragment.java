154239ea47e3fe8156a83d1b5a225da15820f1e8
numerative <michael.hathi@gmail.com>
2018-04-19 21:16:44 +0530

    Home Fragment Layout Overhaul (Based on screens.md)     Use:         Separate layout files for repating views <include> tags for performance.         ConstraintLayout for Responsiveness.         Material Guidelines for Spacing and TextSize.         Best coding practices - strings, styles and dimens.
        Hardcoded strings used only for placeholder text.
    
        Slack ID: @Michael Hathi
    
    Signed-off-by: numerative <michael.hathi@gmail.com>

--- app/src/main/java/com/example/android/audacity/fragments/HomeFragment.java
+++ app/src/main/java/com/example/android/audacity/fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android.audacity.R;
 
@@ -13,6 +14,11 @@ import com.example.android.audacity.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    View rootView, hottestAppCardHeadingView, challengesCardHeadingView, quizzesCardHeadingView,
+            resultsCardHeadingView;
+    TextView hottestAppHeadingTextView, challengesHeadingTextView, quizzesHeadingTextView,
+            resultsHeadingTextView;
 
 
     public HomeFragment() {
@@ -24,7 +30,31 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        setHeadings(); //Sets Text Headings for views like Hottest App of the Day, Challenges, etc.
+
+        return rootView;
     }
 
+    private void setHeadings() {
+
+        //Find All Heading Views
+        hottestAppCardHeadingView = rootView.findViewById(R.id.home_card_heading_hottest_app);
+        challengesCardHeadingView = rootView.findViewById(R.id.home_card_heading_challenges);
+        quizzesCardHeadingView = rootView.findViewById(R.id.home_card_heading_quizzes);
+        resultsCardHeadingView = rootView.findViewById(R.id.home_card_heading_results);
+
+        //Find Corresponding TextViews for each Heading Views above
+        hottestAppHeadingTextView = hottestAppCardHeadingView.findViewById(R.id.card_heading);
+        challengesHeadingTextView = challengesCardHeadingView.findViewById(R.id.card_heading);
+        quizzesHeadingTextView = quizzesCardHeadingView.findViewById(R.id.card_heading);
+        resultsHeadingTextView = resultsCardHeadingView.findViewById(R.id.card_heading);
+
+        //Set Corresponding Titles to the above Heading TextViews
+        hottestAppHeadingTextView.setText(R.string.hottest_app_of_day);
+        challengesHeadingTextView.setText(R.string.challenges);
+        quizzesHeadingTextView.setText(R.string.quizzes);
+        resultsHeadingTextView.setText(R.string.results);
+    }
 }
