75aa097d46f62f965277bc3e973d72afac6af056
lifeLessCoder <ankushshetkar16@gmail.com>
2018-05-03 12:40:50 +0530

    Final POJO classes for hottest app and challenge

--- app/src/main/java/com/example/android/audacity/pojo/Challenge.java
+++ app/src/main/java/com/example/android/audacity/pojo/Challenge.java
@@ -5,33 +5,95 @@ package com.example.android.audacity.pojo;
  */
 
 public class Challenge {
+    // Time stamp for the challenge
+    private String mTimeStamp;
+    // Moderator name
+    private String mModeratorName;
+    // Moderator email address
+    private String mEmail;
+    // Moderator slack id
+    private String mSlackId;
     // Variable to store the challenge name
     private String mChallengeName;
     // Variable to store the challenge description
     private String mChallengeDescription;
+    // Reference Url
+    private String mReferenceUrl;
+    // Image link
+    private String mImageUrl;
+    // Video link
+    private String mVideoUrl;
     // starting date of the challenge
     private String mStartDate;
     // ending date of the challenge
     private String mEndDate;
     // variable to see if the deadline is crossed
     private boolean mIsDeadline;
+    // check if the project is approved
+    private boolean mIsApproved;
 
     /**
      * Constructor
      *
+     * @param timeStamp            time and date of upload
+     * @param moderatorName        name of the moderator
+     * @param email                email of the moderator
+     * @param slackId              slack Id of the moderator
      * @param challengeName        name of the challenge
      * @param challengeDescription description of the challenge
+     * @param referenceUrl         reference url for the challenge
+     * @param imageUrl             image url of the challenge
+     * @param videoUrl             video url of the challenge
      * @param startDate            start date of the challenge
      * @param endDate              end date of the challenge
      * @param isDeadline           is the deadline over or not
+     * @param isApproved           is the project approved
      */
-    public Challenge(String challengeName, String challengeDescription, String startDate,
-                     String endDate, boolean isDeadline) {
+    public Challenge(String timeStamp, String moderatorName, String email, String slackId,
+                     String challengeName, String challengeDescription, String referenceUrl,
+                     String imageUrl, String videoUrl, String startDate, String endDate,
+                     boolean isDeadline, boolean isApproved) {
+        mTimeStamp = timeStamp;
+        mModeratorName = moderatorName;
+        mEmail = email;
+        mSlackId = slackId;
         mChallengeName = challengeName;
         mChallengeDescription = challengeDescription;
+        mReferenceUrl = referenceUrl;
+        mImageUrl = imageUrl;
+        mVideoUrl = videoUrl;
         mStartDate = startDate;
         mEndDate = endDate;
         mIsDeadline = isDeadline;
+        mIsApproved = isApproved;
+    }
+
+    /**
+     * @return the time stamp of the challenge
+     */
+    public String getTimeStamp() {
+        return mTimeStamp;
+    }
+
+    /**
+     * @return the moderator name
+     */
+    public String getModeratorName() {
+        return mModeratorName;
+    }
+
+    /**
+     * @return the email id of the moderator
+     */
+    public String getEmail() {
+        return mEmail;
+    }
+
+    /**
+     * @return the slack id of the challenge
+     */
+    public String getSlackId() {
+        return mSlackId;
     }
 
     /**
@@ -46,6 +108,27 @@ public class Challenge {
      */
     public String getChallengeDescription() {
         return mChallengeDescription;
+    }
+
+    /**
+     * @return the reference url for the challenge
+     */
+    public String getReferenceUrl() {
+        return mReferenceUrl;
+    }
+
+    /**
+     * @return the image url of the challenge
+     */
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    /**
+     * @return the video url of the challenge
+     */
+    public String getVideoUrl() {
+        return mVideoUrl;
     }
 
     /**
@@ -67,5 +150,12 @@ public class Challenge {
      */
     public boolean isDeadline() {
         return mIsDeadline;
+    }
+
+    /**
+     * @return if the project is approved or not
+     */
+    public boolean isApproved() {
+        return mIsApproved;
     }
 }
--- app/src/main/java/com/example/android/audacity/pojo/HottestApp.java
+++ app/src/main/java/com/example/android/audacity/pojo/HottestApp.java
@@ -5,14 +5,28 @@ package com.example.android.audacity.pojo;
  */
 
 public class HottestApp {
+    // Time and date of the upload
+    private String mTimeStamp;
+    // Email id of the student
+    private String mEmail;
+    // Name of the student
+    private String mName;
+    // Slack Id of the student
+    private String mSlackId;
     // Name of the hottest app
     private String mHottestAppName;
     // Description of the hottest app
     private String mHottestAppDescription;
+    // Url of the image of the app(Screenshot)
+    private String mImageUrl;
+    // Url of the video of the hottest app
+    private String mVideoUrl;
     // Github URL of the app
     private String mUrl;
     // Date of upload of the app
     private String mDate;
+    // is the app approved??
+    private boolean mIsApproved;
 
     /**
      * Constructor
@@ -22,11 +36,48 @@ public class HottestApp {
      * @param url                   github link of the hottest app
      * @param date                  date of the app upload
      */
-    public HottestApp(String hottestAppName, String hottestAppDescription, String url, String date) {
+    public HottestApp(String timeStamp, String email, String name, String slackId, String hottestAppName,
+                      String hottestAppDescription, String imageUrl, String videoUrl, String url, String date,
+                      boolean isApproved) {
+        mTimeStamp = timeStamp;
+        mEmail = email;
+        mName = name;
+        mSlackId = slackId;
         mHottestAppName = hottestAppName;
         mHottestAppDescription = hottestAppDescription;
+        mImageUrl = imageUrl;
+        mVideoUrl = videoUrl;
         mUrl = url;
         mDate = date;
+        mIsApproved = isApproved;
+    }
+
+    /**
+     * @return the timestamp of the hottest app
+     */
+    public String getTimeStamp() {
+        return mTimeStamp;
+    }
+
+    /**
+     * @return the email id of the student
+     */
+    public String getEmail() {
+        return mEmail;
+    }
+
+    /**
+     * @return the name of the student
+     */
+    public String getName() {
+        return mName;
+    }
+
+    /**
+     * @return the slack id of the student
+     */
+    public String getSlackId() {
+        return mSlackId;
     }
 
     /**
@@ -44,6 +95,20 @@ public class HottestApp {
     }
 
     /**
+     * @return the image url of the app
+     */
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    /**
+     * @return the video url of the app
+     */
+    public String getVideoUrl() {
+        return mVideoUrl;
+    }
+
+    /**
      * @return github url of the hottest app
      */
     public String getUrl() {
@@ -55,5 +120,12 @@ public class HottestApp {
      */
     public String getDate() {
         return mDate;
+    }
+
+    /**
+     * @return if the app is approved or not
+     */
+    public boolean isApproved() {
+        return mIsApproved;
     }
 }
